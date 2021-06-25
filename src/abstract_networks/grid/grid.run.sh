0c3405aa6e2b1646106a54dec4876cf2ff14201b
ishal17 <ishal17@freeuni.edu.ge>
2021-06-26 01:17:39 +0400

    improved performance with NEAT model
    Co-authored-by: Demetre Pipia <dpipi17@freeuni.edu.ge>
    Co-authored-by: Gega Darakhvelidze <gdara17@freeuni.edu.ge>

--- src/abstract_networks/grid/grid.run.sh
+++ src/abstract_networks/grid/grid.run.sh
@@ -1,4 +1,4 @@
 python3 ../../main.py grid.sumocfg BasicColorBasedScheduler --nogui
 #python3 ../../main.py grid.sumocfg BasicRandomScheduler --nogui  #TODO needs fix
 python3 ../../main.py grid.sumocfg --nogui
-python3 ../../main.py grid.sumocfg NeatScheduler winner.pkl neat_config.txt --nogui
+python3 ../../main.py grid.sumocfg NeatScheduler ../../winner.pkl ../../neat_config.txt --nogui
